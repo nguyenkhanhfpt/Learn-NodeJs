57fd977f06394f799d8926ab2387a3dfe96082f8
Nguyễn Khánh <khanh26122000@gmail.com>
2019-10-29 11:17:43 +0700

    create template pug

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Phonebook')
+    res.render('index')
 });
 
 app.listen(port, () => console.log('Server listen on port: ' +port));
