80206c501a422d00388897a6f2135c23bc085dbe
Nguyễn Khánh <khanh26122000@gmail.com>
2019-10-29 11:29:58 +0700

    add route phonebook

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var listPhonebook = require('./routes/phonebook.route');
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
     res.render('index')
 });
+
+app.use('/phonebook', listPhonebook);
 
 app.listen(port, () => console.log('Server listen on port: ' +port));
