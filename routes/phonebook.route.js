327cd6881810747afb920f510e9898fe47c9bf83
Nguyễn Khánh <khanh26122000@gmail.com>
2019-10-29 11:37:08 +0700

    add controller

--- routes/phonebook.route.js
+++ routes/phonebook.route.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('../db');
+const controller = require('../controller/phonebook.controller');
 
-router.get('/', (req, res) => {
-    res.render('phonebook/index', {
-        list: db.get('list').value()
-    });
-});
+router.get('/', controller.index);
+
 
 
 
