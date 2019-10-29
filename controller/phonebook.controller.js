39e088f73d3c949c1081ace4aeb5bee64dd831b8
Nguyễn Khánh <khanh26122000@gmail.com>
2019-10-29 22:14:22 +0700

    validate form add person

--- controller/phonebook.controller.js
+++ controller/phonebook.controller.js
@@ -26,6 +26,21 @@ module.exports.viewAdd = (req, res) => {
 module.exports.addPerson = (req, res) => {
     req.body.id = shortid.generate();
 
+    let err = [];
+    if (!req.body.name) {
+        err.push('Name is empty!');
+    }
+    if (!req.body.phone) {
+        err.push('Phone is empty!');
+    }
+    if(err.length){
+        res.render('phonebook/viewAdd', {
+            error : err, 
+            values: req.body  // keep values in form after check validate
+        });
+        return;
+    }
+
     db.get('list').push(req.body).write();
 
     res.redirect('/phonebook');
